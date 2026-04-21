204bc0f4a04a269bc2c092c2970bfa37f0ca5dc9
leochenn <leochenn@users.noreply.github.com>
2026-04-21 15:20:04 +0800

    修改 ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ticket-server2',
-      script: 'server.js', // Standalone 模式下的启动入口
+      script: './standalone/server.js', // Standalone 模式下的启动入口
       env: {
         NODE_ENV: 'production',
         PORT: 3001, // 你的应用端口
